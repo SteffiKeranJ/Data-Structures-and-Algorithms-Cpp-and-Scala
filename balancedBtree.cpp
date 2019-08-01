6c8dd63801b76a96de7573d4f5ed46ef6701270e
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 01:46:15 +0530

    110.Balanced Binary Tree
    110.Balanced Binary Tree

---
+++ balancedBtree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+      int depth (TreeNode *root) {
+        if (root == NULL) return 0;
+        return max (depth(root -> left), depth (root -> right)) + 1;
+    }
+
+    bool isBalanced (TreeNode *root) {
+        if (root == NULL) return true;
+        
+        int left=depth(root->left);
+        int right=depth(root->right);
+        
+        return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+};
