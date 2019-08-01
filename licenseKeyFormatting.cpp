93cff20fd136d2cf58a49c8ef59cd3a273a2de3c
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 02:03:47 +0530

    482 	 License Key Formatting
    482 	 License Key Formatting

---
+++ licenseKeyFormatting.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+ string licenseKeyFormatting(string S, int K) {
+        string temp;
+        int i = 0;
+        for (; i < S.length(); i++) {
+            if (S[i]=='-') continue;
+             temp+=toupper(S[i]);
+        }
+        string temp2; int track = 0;
+        for (i = temp.length()-1; i>=0; --i) {
+            track++;
+            temp2+=temp[i];
+            if (track==K && i != 0 ) {
+                temp2+="-";
+                track = 0;
+            }
+        }
+        reverse(temp2.begin(),temp2.end());
+        return temp2;
+    }
+};
