b807595d350b27f1114d47f316beb4940ab1c6ad
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 01:37:55 +0530

    513.Find Bottom Left Tree Value
    513.Find Bottom Left Tree Value

---
+++ findBottomLeftValue.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()) {
+            root=q.front(); // get the value before pop coz pop will not return any val
+            q.pop();
+            if (root->right) {
+                q.push(root->right);
+            }
+            if(root->left) q.push(root->left);
+        }
+        return root->val;
+        
+    }
+};
