eaf129ece9e01f31bf1bbf3c2333f637951d5008
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 01:52:43 +0530

    100 Same Tree
    100 Same Tree

---
+++ isSameTree.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p==NULL && q==NULL) return true;
+        if ((p == NULL && q!=NULL) ||(q == NULL && p!=NULL)) return false;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
