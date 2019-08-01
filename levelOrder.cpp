8d96183dc6aa67c044d094d850e34302f0d014a6
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 01:33:24 +0530

    429.N-ary Tree Level Order Traversal
    429.N-ary Tree Level Order Traversal

---
+++ levelOrder.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) 
+	  {
+     if (!root) 
+      return vector<vector<int>>(); 
+      vector<vector<int>> res; 
+      queue<Node*> q;
+      q.push(root);
+      while (!q.empty())
+      {
+          int size = q.size(); 
+          vector<int> curLevel;
+          for (int i = 0; i < size; i++)
+          {
+              Node* tmp = q.front();
+              q.pop();
+              curLevel.push_back(tmp -> val);
+              for (Node* n : tmp -> children) 
+                   q.push(n); 
+          }
+          res.push_back(curLevel);
+      }
+      return res; 
+    }
+};
