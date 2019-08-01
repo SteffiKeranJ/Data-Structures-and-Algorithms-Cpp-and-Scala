0eea781c143912da34b40b78c53d618b1e96e77f
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-02 01:25:55 +0530

    66.Plus One
    66.Plus One

---
+++ plusOne.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* plusOne(ListNode* head) {
+        if(!head) return NULL;
+        if(helper(head)!=0) {
+            ListNode* newnode = new ListNode(1); // create a new node
+            newnode->next = head;
+            head = newnode;
+        }
+        return head;
+    }
+    
+    int helper(ListNode* node){
+        int carry = node->next ? helper(node->next) : 1;
+        int sum = node->val+carry;
+        node->val = sum%10; // do not store the carry in the node
+        return sum/10;
+    }
+};
