6496014eaebfb053a7ca88d4f28fee26d614c5f5
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-08-04 20:13:32 +0530

    657. Robot Return to Origin
    657. Robot Return to Origin

---
+++ judgeCircle.cpp
@@ -0,0 +1,16 @@
+// Brute force
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int res = 0;
+        for(int i = 0 ; i < moves.length(); i++) {
+            if(moves[i] == 'U') res+=1;
+            else if(moves[i] == 'D') res-=1;
+            else if(moves[i] == 'L') res+=2;
+            else if(moves[i] == 'R') res-=2;
+            else return false;
+        }
+        if(res==0) return true;
+        return false;
+    }
+};
