44023057f3b736438b9a9811a1695bb8d5309101
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2019-12-26 14:22:51 +0530

    Update countDigit.cpp

--- countDigit.cpp
+++ countDigit.cpp
@@ -1,16 +1,13 @@
 class Solution {
 private: 
     int countDigit(int n) {
-        if(n == 0) return 0;
-        return 1+countDigit(n/10);
+        return int(log10(n)+1);
     }    
     
 public:
     int findNumbers(vector<int>& nums) {
         int evenCount = 0;
         for(int i = 0; i < nums.size(); ++i) {
-            int no = nums[i];
-            int t = countDigit(nums[i]);
             if(countDigit(nums[i])%2 == 0) ++evenCount;
         }
         return evenCount;
