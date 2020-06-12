ddbb60f2a9ed5d2cfe4ebca76316b61b42a8cdc5
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2020-06-13 02:00:19 +0530

    Check if the ranges are overlapping | If no, return false

---
+++ Greedy/canAttendMeetings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if(intervals.size()==0) return true;
+        sort(intervals.begin(), intervals.end());
+         for(int i = 0; i < intervals.size()-1; ++i) {
+            if(intervals[i][1] > intervals[i+1][0]) {
+                // not gonna overlap
+                return false;
+            }
+         }
+        return true;
+    }
+};
