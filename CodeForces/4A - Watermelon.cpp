ceef1c0eb517adcddf69efbc696dd19c1464e2ea
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2021-04-18 23:53:17 +0530

    Create 4A - Watermelon.cpp

---
+++ CodeForces/4A - Watermelon.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+ 
+int main() {
+    int T;
+    cin >> T;
+    if(T > 2 && T%2==0) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
