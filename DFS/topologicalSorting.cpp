444accac0dd88a8022581d742e66d9987ce81db1
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2021-07-01 01:33:27 +0530

    Update topologicalSorting.cpp

--- DFS/topologicalSorting.cpp
+++ DFS/topologicalSorting.cpp
@@ -1,3 +1,13 @@
+/**
+Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+**/
+
+
 class Solution {
 public:
     int color[2001] = {0};
