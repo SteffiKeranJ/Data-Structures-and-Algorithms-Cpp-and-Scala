7b021d87134c3f046af98a0dc3a4c325ffff1b6b
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2021-08-04 02:14:19 +0530

    Create numDistinct.cpp

---
+++ Dynamic-Programming/numDistinct.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        if (s.empty() || t.empty() || s.size() < t.size()) return 0;
+        vector<vector<unsigned long long int>> dp(m+1, vector<unsigned long long int> (n+1, 0));
+        
+        for(int i = 0; i <= m; ++i) {
+            dp[i][0]=1;
+        }
+        
+        for(int i = 1; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                dp[i][j] = dp[i-1][j];
+                 if(s[i-1] == t[j-1]) {
+                      dp[i][j] += dp[i-1][j-1];
+                 }
+            }
+        }
+        return dp[m][n];
+    }
+};
