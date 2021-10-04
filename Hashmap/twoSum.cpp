4e17ad8b5fffa50cd2a189620ca8aabc9c7c5d2f
Steffi Keran Rani J <SteffiKeranJ@users.noreply.github.com>
2021-10-04 10:43:47 -0700

    Update twoSum.cpp

--- Hashmap/twoSum.cpp
+++ Hashmap/twoSum.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> map;
-        int n = nums.size();
-        vector<int> m;
-        for(int i = 0; i < n; ++i) {
-           int rem = target - nums[i];
-            if(map.find(rem)!= map.end()){
-                m.push_back(map[rem]);
-                m.push_back(i);
-                return m;
-            }
-            map[nums[i]] = i;
+        unordered_map<int, int> hmap;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(hmap.find(target-nums[i]) != hmap.end()) 
+                return {hmap[target-nums[i]], i};
+            hmap[nums[i]] = i;
         }
-        return m;
+        
+        return {-1,-1};
     }
 };
